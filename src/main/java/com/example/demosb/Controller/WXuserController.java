75facc34690c0c6134f348ea104408660247fe9c
firstcapf <172128788@qq.com>
2018-09-19 16:52:17 +0800

    微信绑定成绩查询

--- src/main/java/com/example/demosb/Controller/WXuserController.java
+++ src/main/java/com/example/demosb/Controller/WXuserController.java
@@ -29,6 +29,12 @@ public class WXuserController {
     @Resource
     private WXuserService wxuserService;
 
+
+    /**
+     *成绩查询接口  getcode
+     * url:  https://open.weixin.qq.com/connect/oauth2/authorize?appid=wxe68e474aba8ac509&redirect_uri=http%3a%2f%2flandbigdata.swjtu.edu.cn%2fgetcode&response_type=code&scope=snsapi_userinfo&state=123&connect_redirect=1#wechat_redirect
+     * 下一步签到接口
+     */
     @RequestMapping("/getcode")
     public  String  getcode(HttpServletRequest request,HttpServletResponse response, Map<String,Object> model) throws KeyManagementException, NoSuchAlgorithmException, NoSuchProviderException, IOException {
 
@@ -51,6 +57,42 @@ public class WXuserController {
           response.sendRedirect("http://landbigdata.swjtu.edu.cn/score/#/");
           return token.getOpenId();
     }
+
+
+    /**
+     * 下一步完成签到接口
+     * @param request
+     * @param response
+     * @param model
+     * @return
+     * @throws KeyManagementException
+     * @throws NoSuchAlgorithmException
+     * @throws NoSuchProviderException
+     * @throws IOException
+     */
+    @RequestMapping("/signin")
+    public  String  signin(HttpServletRequest request,HttpServletResponse response, Map<String,Object> model) throws KeyManagementException, NoSuchAlgorithmException, NoSuchProviderException, IOException {
+
+        String code =request.getParameter("code");
+        WeixinOauth2Token token=CommonUtil.getOauth2AccessToken(code);
+        SNSUserInfo userinfo=CommonUtil.getSNSUserInfo(token.getAccessToken(),token.getOpenId());
+        if(userinfo!=null&&userinfo.getOpenId()!=null)
+        {
+            WXuser wxuser=new WXuser();
+            wxuser.setOpenId(userinfo.getOpenId());
+            wxuser.setNickname(userinfo.getNickname());
+            wxuser.setProvince(userinfo.getProvince());
+            wxuser.setCountry(userinfo.getCountry());
+            wxuser.setSex(userinfo.getSex());
+            wxuser.setHeadimgurl(userinfo.getHeadImgUrl());
+            wxuserService.insertWXuser(wxuser);
+            request.getSession().setAttribute("openid", token.getOpenId());
+        }
+        //重定向跳转到查询页面
+        response.sendRedirect("http://landbigdata.swjtu.edu.cn/score/#/");
+        return token.getOpenId();
+    }
+
 
     @RequestMapping("/wxuseradd")
     public String wxuseradd(HttpServletRequest request, Map<String,Object> model) throws KeyManagementException, NoSuchAlgorithmException, NoSuchProviderException, IOException {
